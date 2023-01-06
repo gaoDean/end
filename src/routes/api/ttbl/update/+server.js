dcd894a949d8645e6e1e32776048616b23bfcf78
Dean <gao.dean@hotmail.com>
2023-01-06 21:37:46 +1100

    refactor(ttbl): simplify to test

--- src/routes/api/ttbl/update/+server.js
+++ src/routes/api/ttbl/update/+server.js
@@ -4,44 +4,44 @@ const res204 = new Response(null, {
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET({ url }) {
-	const targetVersion = url.searchParams.get('target');
+	// const targetVersion = url.searchParams.get('target');
 
 	const releases = await fetch(
 		'https://api.github.com/repos/gaoDean/ttbl/releases',
 	);
 	if (!releases || !releases.ok) return res204;
 
-	const release = (await releases.json()).find(
-		(x) => x.tag_name === targetVersion,
-	);
-	if (!release) return res204;
+	// const release = (await releases.json()).find(
+	// 	(x) => x.tag_name === targetVersion,
+	// );
+	// if (!release) return res204;
+	//
+	// const assetsMacIntel = release.assets.find(
+	// 	(x) => x.name === 'ttbl-intel_tarball.tar.gz',
+	// );
+	// const assetsMacSilicon = release.assets.find(
+	// 	(x) => x.name === 'ttbl-m1_tarball.tar.gz',
+	// );
+	//
+	// if (!assetsMacIntel || !assetsMacSilicon) return res204;
+	//
+	// const ret = {
+	// 	version: targetVersion,
+	// 	notes: release.body,
+	// 	pub_date: release.published_at,
+	// 	platforms: {
+	// 		'darwin-x86_64': {
+	// 			signature: '',
+	// 			url: assetsMacIntel.url,
+	// 		},
+	// 		'darwin-aarch64': {
+	// 			signature: '',
+	// 			url: assetsMacSilicon.url,
+	// 		},
+	// 	},
+	// };
 
-	const assetsMacIntel = release.assets.find(
-		(x) => x.name === 'ttbl-intel_tarball.tar.gz',
-	);
-	const assetsMacSilicon = release.assets.find(
-		(x) => x.name === 'ttbl-m1_tarball.tar.gz',
-	);
-
-	if (!assetsMacIntel || !assetsMacSilicon) return res204;
-
-	const ret = {
-		version: targetVersion,
-		notes: release.body,
-		pub_date: release.published_at,
-		platforms: {
-			'darwin-x86_64': {
-				signature: '',
-				url: assetsMacIntel.url,
-			},
-			'darwin-aarch64': {
-				signature: '',
-				url: assetsMacSilicon.url,
-			},
-		},
-	};
-
-	return new Response(ret, {
+	return new Response(releases, {
 		status: 200,
 	});
 }
