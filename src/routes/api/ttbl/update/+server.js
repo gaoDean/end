f527b93167291c9ec67fb143fa7386c6561a4925
Dean <gao.dean@hotmail.com>
2023-01-06 20:35:04 +1100

    feat: change slug based endpoint to params based

--- src/routes/api/ttbl/update/+server.js
+++ src/routes/api/ttbl/update/+server.js
@@ -1,9 +1,7 @@
 /** @type {import('./$types').RequestHandler} */
 export function GET({ url }) {
 	const target = url.searchParams.get('target');
-	const currentVersion = url.searchParams.get('currentVersion');
-
-
+	const currentVersion = url.searchParams.get('current');
 
 	return new Response(`${target}/${currentVersion}`);
 }
