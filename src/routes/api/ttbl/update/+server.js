deec404d00a8845e0e35e5c09b1a29d2d06e11da
Dean <gao.dean@hotmail.com>
2023-01-06 21:24:42 +1100

    feat: get the correct update

--- src/routes/api/ttbl/update/+server.js
+++ src/routes/api/ttbl/update/+server.js
@@ -1,12 +1,43 @@
 /** @type {import('./$types').RequestHandler} */
 export function GET({ url }) {
 	const targetVerson = url.searchParams.get('target');
-	const currentVersion = url.searchParams.get('current');
 
-	const ret = {
-		targetVersion,
-		currentVersion,
+	const releases = fetch("https://api.github.com/repos/gaoDean/ttbl/releases")
+
+	const release = releases.find((x) => x.tag_name === targetVersion)
+
+	if (!release) {
+		return new Response(null, {
+			status: 204,
+		})
 	}
 
-	return new Response(ret);
+	const assetsMacIntel = release.assets.find((x) => x.name === "ttbl-intel_tarball.tar.gz")
+	const assetsMacSilicon = release.assets.find((x) => x.name === "ttbl-m1_tarball.tar.gz")
+
+	if (!assetsMacIntel || !assetsMacSilicon) {
+		return new Response(null, {
+			status: 204,
+		})
+	}
+
+	const ret = {
+		version: targetVersion,
+		notes: release.body,
+		pub_date: release.published_at,
+		platforms: {
+			darwin-x86_64: {
+				signature: "",
+				url: assetsMacIntel.url
+			},
+			darwin-aarch64: {
+				signature: "",
+				url: assetsMacSilicon.url
+			},
+		}
+	}
+
+	return new Response(ret, {
+		status: 200,
+	});
 }
