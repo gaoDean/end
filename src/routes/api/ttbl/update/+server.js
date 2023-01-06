3edcda68d37bc2192d79aad492cc90c51bcd778c
Dean <gao.dean@hotmail.com>
2023-01-06 21:29:17 +1100

    fix(ttbl): json fixes

--- src/routes/api/ttbl/update/+server.js
+++ src/routes/api/ttbl/update/+server.js
@@ -1,24 +1,28 @@
 /** @type {import('./$types').RequestHandler} */
 export function GET({ url }) {
-	const targetVerson = url.searchParams.get('target');
+	const targetVersion = url.searchParams.get('target');
 
-	const releases = fetch("https://api.github.com/repos/gaoDean/ttbl/releases")
+	const releases = fetch('https://api.github.com/repos/gaoDean/ttbl/releases');
 
-	const release = releases.find((x) => x.tag_name === targetVersion)
+	const release = releases.find((x) => x.tag_name === targetVersion);
 
 	if (!release) {
 		return new Response(null, {
 			status: 204,
-		})
+		});
 	}
 
-	const assetsMacIntel = release.assets.find((x) => x.name === "ttbl-intel_tarball.tar.gz")
-	const assetsMacSilicon = release.assets.find((x) => x.name === "ttbl-m1_tarball.tar.gz")
+	const assetsMacIntel = release.assets.find(
+		(x) => x.name === 'ttbl-intel_tarball.tar.gz',
+	);
+	const assetsMacSilicon = release.assets.find(
+		(x) => x.name === 'ttbl-m1_tarball.tar.gz',
+	);
 
 	if (!assetsMacIntel || !assetsMacSilicon) {
 		return new Response(null, {
 			status: 204,
-		})
+		});
 	}
 
 	const ret = {
@@ -26,16 +30,16 @@ export function GET({ url }) {
 		notes: release.body,
 		pub_date: release.published_at,
 		platforms: {
-			darwin-x86_64: {
-				signature: "",
-				url: assetsMacIntel.url
+			'darwin-x86_64': {
+				signature: '',
+				url: assetsMacIntel.url,
 			},
-			darwin-aarch64: {
-				signature: "",
-				url: assetsMacSilicon.url
+			'darwin-aarch64': {
+				signature: '',
+				url: assetsMacSilicon.url,
 			},
-		}
-	}
+		},
+	};
 
 	return new Response(ret, {
 		status: 200,
