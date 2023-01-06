f219a1fa419a6675834b9276a2bd8af988dbccaf
Dean <gao.dean@hotmail.com>
2023-01-06 20:44:06 +1100

    feat: test return json

--- src/routes/api/ttbl/update/+server.js
+++ src/routes/api/ttbl/update/+server.js
@@ -1,7 +1,12 @@
 /** @type {import('./$types').RequestHandler} */
 export function GET({ url }) {
-	const target = url.searchParams.get('target');
+	const targetVerson = url.searchParams.get('target');
 	const currentVersion = url.searchParams.get('current');
 
-	return new Response(`${target}/${currentVersion}`);
+	const ret = {
+		targetVersion,
+		currentVersion,
+	}
+
+	return new Response(ret);
 }
